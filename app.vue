4d6389dcf44874130efd101743b50152f9193a87
safarov-en <safarov.elkhan110@gmail.com>
2024-06-12 15:51:29 +0500

    Adding posts

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     <NuxtPage></NuxtPage>
   </NuxtLayout>
 </template>
+<script setup>
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+const layout = "main-content";
+const nuxtApp = useNuxtApp()
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  nuxtApp.vueApp.component(key, component)
+}
+</script>
--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,15 +1,56 @@
 <template>
-    <article class="post"></article>
+    <el-card
+        shadow="hover"
+        :body-style="{padding: 0}"
+        class="post"
+    >
+        <template #header>
+            <header class="post-header">
+                <h3></h3>
+                <small>
+                    <el-icon><AlarmClock /></el-icon>
+                    {{new Date().toLocaleString()}}
+                </small>
+            </header>
+        </template>
+        <div class="post-body">
+            <img src="" alt="" class="post-img" />
+        </div>
+        <footer class="post-footer">
+            <el-button round @click="openPost">Открыть</el-button>
+            <span>
+                <el-icon><Message /></el-icon>
+                12
+            </span>
+        </footer>
+    </el-card>
 </template>
 <script>
 export default {
-    
+    methods: {
+        openPost() {
+
+        }
+    }
 }
 </script>
 <style lang="scss" scoped>
     .post {
-        height: 200px;
-        border: 1px solid black;
-        margin-bottom: 2rem;
+        margin-bottom: 1.5rem;
+    }
+    .post-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .post-img {
+        width: 100%;
+        height: auto;
+    }
+    .post-footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
     }
 </style>
