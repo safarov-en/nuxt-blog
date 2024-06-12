401d9e0652d444a0107dbf666503a4d64d8684c4
safarov-en <safarov.elkhan110@gmail.com>
2024-06-12 13:26:14 +0500

    Adding the main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div></div>
+    <el-row type="flex" justify="center">
+        <el-col :xs="24" :sm="18" :md="12" :lg="10">
+            <main-post
+                v-for="post in 3"
+                :key="post"
+            />
+        </el-col>
+    </el-row>
 </template>
 <script>
 </script>
