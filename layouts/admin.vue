228f92de816460a46010a79aff7c9ac8084e8a91
safarov-en <safarov.elkhan110@gmail.com>
2024-06-13 12:18:23 +0500

    Adding an admin page

---
+++ layouts/admin.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="admin-layout-wrap">
+        <el-container :style="{height: '100%'}">
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>
+                <slot />
+            </el-main>
+        </el-container>
+    </div>
+</template>
+<script>
+</script>
+<style lang="scss" scoped>
+    .admin-layout-wrap {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
